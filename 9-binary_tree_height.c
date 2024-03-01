3708b12c7bc3c61bbc4563bd9709baf936ef9f86
Code021-raelle <gabrielakinshola3@gmail.com>
2024-03-01 17:29:39 +0100

    binary

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include <stdio.h>
 
 /**
  * binary_tree_height - Measures the height of a binary tree
--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,9 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
